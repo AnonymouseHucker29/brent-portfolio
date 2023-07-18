8d439d998d00b2163688d2997b54ed70bc5ea098
AnonymouseHucker29 <brent.baylon29@gmail.com>
2023-07-18 22:17:07 +0800

    redesigned homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+'use client'
+
+import * as React from "react";
 import Layout from "@/components/Layout";
+import TypewriterComponent from "typewriter-effect";
 
 export default function Home() {
   return (
     <>
       <Layout>
         <div className="flex justify-center items-center h-screen text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
-          &nbsp;&nbsp;&nbsp;Hello there 👋
-          <br />
-          I&apos;m Brent Baylon
+          <TypewriterComponent
+            options={{
+              strings: ["Hello there! :)", "I'm Brent Baylon", "an aspiring Web Developer"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
       </Layout>
     </>

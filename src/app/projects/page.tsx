8c3d5197df0fe11efa78a493517c0dab19fc76c5
Brent Baylon <brent.baylon29@gmail.com>
2023-08-01 17:34:21 +0800

    minor typo

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-    title: "Project | Brent Baylon",
+    title: "Projects | Brent Baylon",
     description: "Personal Portfolio",
     icons: "/icons/logo.svg",
 }

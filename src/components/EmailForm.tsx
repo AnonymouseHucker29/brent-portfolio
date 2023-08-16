519f7f117b1cb93e47c0ab54b7ef70e80a5cbc95
AnonymouseHucker29 <brent.baylon29@gmail.com>
2023-08-16 22:31:44 +0800

    changed input to textarea in message field

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -9,9 +9,10 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast, ToastContainer } from "react-toastify";
 import { useState } from "react";
-import { CheckCircledIcon } from "@radix-ui/react-icons";
+import { CheckCircledIcon, ReloadIcon } from "@radix-ui/react-icons";
 import { avantgarde } from "@/app/layout";
 import { useTheme } from "next-themes";
+import { Textarea } from "@/components/ui/textarea";
 import {
   Form,
   FormControl,
@@ -95,11 +96,19 @@ export default function ProfileForm() {
                     {fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}
                   </FormLabel>
                   <FormControl>
-                    <Input
-                      className="border-purple-600 dark:border-purple-600"
-                      placeholder={`Enter your ${fieldName}`}
-                      {...field}
-                    />
+                    {fieldName === "message" ? (
+                      <Textarea
+                        className="border-purple-600 dark:border-purple-600 resize-none"
+                        placeholder={`Enter your ${fieldName}`}
+                        {...field}
+                      />
+                    ) : (
+                      <Input
+                        className="border-purple-600 dark:border-purple-600"
+                        placeholder={`Enter your ${fieldName}`}
+                        {...field}
+                      />
+                    )}
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -111,33 +120,14 @@ export default function ProfileForm() {
               variant="outline"
               type="submit"
               disabled={[isSubmitting, submitText === "check"].some(Boolean)}
-              className="border-purple-600 dark:border-purple-600 md:hover:bg-purple-600 md:dark:hover:bg-purple-600 lg:hover:bg-purple-600 lg:dark:hover:bg-purple-600 xl:hover:bg-purple-600 xl:dark:hover:bg-purple-600"
+              className="rounded-full w-full border-purple-600 dark:border-purple-600 md:hover:bg-purple-600 md:dark:hover:bg-purple-600 lg:hover:bg-purple-600 lg:dark:hover:bg-purple-600 xl:hover:bg-purple-600 xl:dark:hover:bg-purple-600"
             >
               {isSubmitting ? (
-                <svg
-                  className="animate-spin h-5 w-5 text-purple-600 dark:text-purple-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zm9-1.37l3 2.646C19.865 17.825 21 15.043 21 12h-4a7.96 7.96 0 01-2 4.92zM7 6.708l3 2.647V3.061L7 6.708z"
-                  ></path>
-                </svg>
+                <ReloadIcon className="animate-spin" />
               ) : submitText === "check" ? (
-                <CheckCircledIcon color="#af4bfb" />
+                <CheckCircledIcon />
               ) : (
-                "Send"
+                "SEND"
               )}
             </Button>
           </div>
